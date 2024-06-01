1303c734fa73386593171f4f2047cf2658b2e041
Nick Sanchez <nicksanchez2371@gmail.com>
2024-05-31 23:41:36 -0500

    issue was due to b in getboundingClientRect not being captialize

--- ball.js
+++ ball.js
@@ -1,6 +1,6 @@
 
-const INITIAL_VELOCITY = .025;
-const VELOCITY_INCREASE = .00001
+const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001
 
 export default class Ball {
     constructor(ballElem) {
@@ -27,7 +27,7 @@ export default class Ball {
     }
 
     rect() {
-        return this.ballElem.getboundingClientRect()
+        return this.ballElem.getBoundingClientRect()
     }
 
     reset() {
@@ -47,7 +47,7 @@ export default class Ball {
     update(delta) {
         this.x += this.direction.x * this.velocity * delta
         this.y += this.direction.y * this.velocity * delta
-        this.velocity = VELOCITY_INCREASE * delta
+        this.velocity += VELOCITY_INCREASE * delta
         const rect = this.rect()
 
         if(rect.bottom >= window.innerHeight || rect.top <= 0) {
