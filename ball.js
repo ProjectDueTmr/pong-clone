41d2d57698d62df88fdf9ced31ac5a35d37675a8
Nick Sanchez <nicksanchez2371@gmail.com>
2024-05-31 23:31:58 -0500

    error with this.ballElem.getboundingClientRect not being a function

--- ball.js
+++ ball.js
@@ -1,5 +1,6 @@
 
 const INITIAL_VELOCITY = .025;
+const VELOCITY_INCREASE = .00001
 
 export default class Ball {
     constructor(ballElem) {
@@ -25,6 +26,10 @@ export default class Ball {
         this.ballElem.style.setProperty("--y", value)
     }
 
+    rect() {
+        return this.ballElem.getboundingClientRect()
+    }
+
     reset() {
         this.x = 50;
         this.y = 50;
@@ -42,6 +47,15 @@ export default class Ball {
     update(delta) {
         this.x += this.direction.x * this.velocity * delta
         this.y += this.direction.y * this.velocity * delta
+        this.velocity = VELOCITY_INCREASE * delta
+        const rect = this.rect()
+
+        if(rect.bottom >= window.innerHeight || rect.top <= 0) {
+            this.direction.y *= -1
+        }
+        if(rect.right >= window.innerHeight || rect.left <= 0) {
+            this.direction.x *= -1
+        }
     }
 }
 
